7f82f9a05ec41df34f14958c45399d2a7f9ba757
cgcb-01 <206439682+cgcb-01@users.noreply.github.com>
2025-06-23 13:57:24 +0530

    Learned till strings

--- index.py
+++ index.py
@@ -1,3 +1,20 @@
+# Arithmatic operator + addition, - subtraction, * multiplication, / division, % modulo , ** exponent:2**3=8
+# For rounding the value of a division we can use //
 
+pow_result = 2**4
+print(pow_result)
 
-print("Hello World")
+# input function to take input
+user_inp = input("Enter Something: ")
+print("You Entered:", user_inp)
+
+# Strings and its Formatting
+# doublr Quotes:"" or Single Quotes:'', For Multi line use:''' triple single quotes'''.
+multiline = """I am Sutapa.
+And I am 19 years old"""
+print(multiline)
+
+# formated string with f
+name = "sutapa"
+print(f"My name is: {name}")
+print(f"2+2={2+2}")
